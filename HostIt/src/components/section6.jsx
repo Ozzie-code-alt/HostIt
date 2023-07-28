547caeed82e468297d58d5c6bbc2687e6f01ccca
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-07-29 02:09:12 +0800

    some changes

--- HostIt/src/components/section6.jsx
+++ HostIt/src/components/section6.jsx
@@ -3,17 +3,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import Carousel from 'react-bootstrap/Carousel';
 import { TESTIMONIALS } from '../testimonial'
+import './Styles/section6.css'
+
 function Section6() {
   return (
         <>  
         <Carousel>
             {TESTIMONIALS.map((testimonial) =>
             <Carousel.Item key ={testimonial.id}>
+    
             <div className="testimonial-item">
             <p className="testimonial-comment">{testimonial.comment}</p>
             <p className="testimonial-name">{testimonial.name}</p>
             <p className="testimonial-role">{testimonial.role}</p>
+         
           </div>
+        
             </Carousel.Item>)}
         </Carousel>
         
