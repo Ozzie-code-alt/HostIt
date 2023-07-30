40f0be2fe45a493bba4513f46f4d8e34444412bb
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-07-30 23:31:21 +0800

    MoreQueries

--- HostIt/src/components/Navbar.jsx
+++ HostIt/src/components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import './Styles/Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+
 function Navbar() {
+
+const [show , setShow] = useState(false)
+
+const handleClick = ()=>{
+setShow(!show)
+}
   return (
     <div className='nav-container'>
         <nav>
@@ -18,6 +27,25 @@ function Navbar() {
                 <li> <FontAwesomeIcon icon={faMagnifyingGlass} /> </li>
                 <li>CALL: 09123-456-789</li>
             </ul>
+
+            <div className="nav-btn">
+                <button onClick={handleClick}><FontAwesomeIcon icon = {faBars} /></button>{
+                   show &&(
+                    <div className="container-show">
+                    <ul >
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>SERVICES</li>
+                    <li>PRICING</li>
+                    <li>CONTACT US</li>
+                    <li> <FontAwesomeIcon icon={faMagnifyingGlass} /> </li>
+                    <li>CALL: 09123-456-789</li>                 
+                </ul>
+                </div>
+                    )
+                }
+            </div>
+            
         </nav>
         </div>
   )
